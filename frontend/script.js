f0d6f2bd087e705dffbe57a6398c2e9ed8de20da
Onur Tosuner <128583602+azazel1805@users.noreply.github.com>
2025-07-16 15:36:23 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loadingTextElem = document.getElementById('loading-text');
 
     // IMPORTANT: Replace with your deployed backend URL in production
-    const BACKEND_URL = 'http://localhost:3000'; 
+    const BACKEND_URL = '';  
 
     let recognition;
     let isRecording = false;
